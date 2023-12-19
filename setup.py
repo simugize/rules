3f4a1bf838cc1d3214ecc0dd19919ef9fe3e4284
saplets <sapletsinfo@gmail.com>
2023-12-19 13:12:43 -0800

    Added setup py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rules',
+    version='0.1',
+    packages=find_packages(),
+    # Add more properties like 'install_requires', 'url', etc.
+)
