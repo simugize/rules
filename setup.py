1242d2a3d0579808cb3af9db86b5bd92d208523b
saplets <sapletsinfo@gmail.com>
2023-12-19 13:55:54 -0800

    Increment the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yaml_rules',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     # Add more properties like 'install_requires', 'url', etc.
 )
